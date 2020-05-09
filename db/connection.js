661256aa50b2ec06a5a490f715175a684ca7d402
Artem Korensky <a.korensky@brainq.com>
2020-05-09 19:14:14 +0300

    qweqwe

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,14 @@
 const knex = require("knex");
 const knexDb = knex({
   client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-  },
+  // connection: {
+  //   host: process.env.DB_HOST,
+  //   user: process.env.DB_USERNAME,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_NAME,
+  //   port: process.env.DB_PORT,
+  // },
+  connection: process.env.DATABASE_URL,
 });
 const bookshelf = require("bookshelf");
 const securePassword = require("bookshelf-secure-password");
--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,13 @@ require("dotenv").config();
 module.exports = {
   development: {
     client: "pg",
-    connection: {
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT,
-    },
+    // connection: {
+    //   database: process.env.DB_NAME,
+    //   user: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   port: process.env.DB_PORT,
+    // },
+    connection: process.env.DATABASE_URL,
   },
   migrations: {
     tableName: "knex_migrations",
